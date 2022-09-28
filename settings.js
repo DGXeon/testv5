3b361461181757933668c3f202cd81b2a897b3e0
Xeon <88829846+DGXeon@users.noreply.github.com>
2022-09-28 06:44:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -69,7 +69,7 @@ global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.packname = "Cheems Bot Stickers\n " //ur sticker watermark packname
 global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
 global.wm = "Cheems Bot-MD Presents"
 
